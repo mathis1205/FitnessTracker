2f9aee0338eb6d16b094bcea52c638e5b721aeea
mathis1205 <96077034+mathis1205@users.noreply.github.com>
2024-03-19 08:29:39 +0100

    bing bong

--- Models/VoedingAPI.cs
+++ Models/VoedingAPI.cs
@@ -1,34 +1,30 @@
 ﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace FitnessTracker.Models
+namespace FitnessTracker.Models;
+
+public class VoedingAPI
 {
-    public class VoedingAPI
+    public async Task<Voeding> GetRecipe(Voeding voeding)
     {
-        public async Task<Voeding> GetRecipe(Voeding voeding)
-        {
-            using var client = new HttpClient();
-            var requestUri = "https://api.spoonacular.com/recipes/complexSearch?$";
-            requestUri += voeding.query != null ? $"&query={voeding.query}" : "";
-            requestUri += voeding.intolerance != null ? $"&intolerances={voeding.intolerance}" : "";
-            requestUri += voeding.cuisine != null ? $"&cuisine={voeding.cuisine}" : "";
-            requestUri += voeding.diet != null ? $"&diet={voeding.diet}" : "";
-            requestUri += "&apiKey=568e8dd7bb494bae980d7a87b9979867";
+        using var client = new HttpClient();
+        var requestUri = "https://api.spoonacular.com/recipes/complexSearch?$";
+        requestUri += voeding.query != null ? $"&query={voeding.query}" : "";
+        requestUri += voeding.intolerance != null ? $"&intolerances={voeding.intolerance}" : "";
+        requestUri += voeding.cuisine != null ? $"&cuisine={voeding.cuisine}" : "";
+        requestUri += voeding.diet != null ? $"&diet={voeding.diet}" : "";
+        requestUri += "&apiKey=568e8dd7bb494bae980d7a87b9979867";
 
-            var response = await client.GetAsync(requestUri);
+        var response = await client.GetAsync(requestUri);
 
-            var jsonString = await response.Content.ReadAsStringAsync();
-            var model = JsonConvert.DeserializeObject<RootVoeding>(jsonString);
-            List<Voeding> recipes = model.results;
-            Voeding? _voeding = recipes.FirstOrDefault();
-            return _voeding;
-        }
+        var jsonString = await response.Content.ReadAsStringAsync();
+        var model = JsonConvert.DeserializeObject<RootVoeding>(jsonString);
+        var recipes = model.results;
+        var _voeding = recipes.FirstOrDefault();
+        return _voeding;
     }
+}
 
-    public class RootVoeding
-    {
-        public List<Voeding> results { get; set; }
-    }
+public class RootVoeding
+{
+    public List<Voeding> results { get; set; }
 }
--- Models/VoedingPage.cs
+++ Models/VoedingPage.cs
@@ -1,12 +1,12 @@
-﻿namespace FitnessTracker.Models
+﻿namespace FitnessTracker.Models;
+
+public class VoedingPage
 {
-	public class VoedingPage
-	{
-		public IList<Voeding> _Voeding { get; set; }
-		public Voeding Voeding { get; set; }
-		public VoedingPage(IList<Voeding> _Voeding)
-		{
-			this._Voeding = _Voeding;
-		}
-	}
+    public VoedingPage(IList<Voeding> _Voeding)
+    {
+        this._Voeding = _Voeding;
+    }
+
+    public IList<Voeding> _Voeding { get; set; }
+    public Voeding Voeding { get; set; }
 }
